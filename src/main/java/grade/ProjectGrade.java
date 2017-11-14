ddffe5e43e283194e28aab29c2a950392d922b1a
mjwsteenbergen <martijnsreclame@hotmail.nl>
2017-11-14 22:10:18 +0100

    Created lambda, which returned false. Fixed code

--- src/main/java/grade/ProjectGrade.java
+++ src/main/java/grade/ProjectGrade.java
@@ -21,7 +21,7 @@ public class ProjectGrade {
     }
 
     public static boolean usesLambda() {
-        return IntStream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(i -> i < 9).allMatch(i -> i > 9);
+        return IntStream.of(0, 2, 4, 6, 8, 10).map(i -> i*i).allMatch(i -> (i + 1)%2 == 1);
     }
 
     /**
