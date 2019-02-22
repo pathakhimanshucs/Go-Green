c6845fd768b91873e87e6deb2ca5616049829ffd
Michael Chan <J.M.Chan@student.tudelft.nl>
2019-02-22 14:51:45 +0100

    Web API now running

--- src/main/java/Server/WebAPI.java
+++ src/main/java/Server/WebAPI.java
@@ -1,14 +1,23 @@
 package Server;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebAPI {
 
-    public static void main(String[] args)
 
+
+    @RequestMapping("/request")
+    public String Request(@RequestParam(value="name", defaultValue="World") String name)
     {
 
 
 
+        return "Hello " + name;
     }
+
 
 
 
