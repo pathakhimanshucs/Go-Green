2022c21946b27d88a0099ac3fb5733dc4e54e66f
Michael Chan <J.M.Chan@student.tudelft.nl>
2019-02-22 15:43:18 +0100

    Client now sends http request and prints String.

---
+++ src/main/java/Client/Application.java
@@ -0,0 +1,43 @@
+package Client;
+
+import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Application
+{
+
+    public static void main(String[] args)
+    {
+
+        Request();
+
+    }
+
+    public static void Request()
+    {
+        try
+        {
+            String output;
+            URL url = new URL("http://localhost:8080/request");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            //BufferedReader Reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            Scanner sc = new Scanner(con.getInputStream());
+            output = sc.nextLine();
+
+            System.out.println(output);
+
+          //  con.disconnect();
+
+
+        }
+        catch(IOException e)
+        {
+            System.exit(1);
+        }
+    }
+
+}
