73b4984ba98ae7c542d4a1d4ca5c9487ad51b915
tamersahin61 <t.sahin@student.tudelft.nl>
2019-03-16 21:07:56 +0100

    Latest GUI version

--- src/main/java/client/Login.java
+++ src/main/java/client/Login.java
@@ -1,16 +1,14 @@
 package client;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,9 +18,6 @@ import javax.swing.JTextField;
 public class Login {
 
     public static void main(String[] args) {
-
-
-/////////////////////////////////////////// FRAME 1 /////////////////////////////////////////////////
 
 
         JFrame newAccount = new JFrame("Create new account");
@@ -57,7 +52,7 @@ public class Login {
         JButton pressCreateAccount = new JButton("Create account");
         pressCreateAccount.setBounds(50,325,150,30);
 
-        JButton LoginChoice = new JButton("login");
+        JButton LoginChoice = new JButton("Login");
         LoginChoice.setBounds(230,325,160,30);
 
         JPanel panel2 = new JPanel();
@@ -88,24 +83,28 @@ public class Login {
                 String password = enterPassword.getText();
                 System.out.println(username);
                 System.out.println(password);
-                System.out.println("Welcome " + Application.loginToServer(username, password).getName());
 
             }});
 
+        pressCreateAccount.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                String username =enterUsername.getText();
+                String password = enterPassword.getText();
+                System.out.println(username);
+                System.out.println(password);
+
+            }});
 
         pressCreateAccount.addActionListener(new Action2());
-        //LoginChoice.addActionListener(new Action1());
+
         newAccount.add(panel2); //Add the features in the panel into the frame
 
 //			    if (LoginChoice.getModel().isPressed()) {
 //			    	//newAccount.dispose();
 //			    	newAccount.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-//			    }
-
     }
-
-////////////////////////////////// FRAME IF CREATE AN ACCOUNT IS CHOSEN //////////////////////////////
-
 
     static class Action2 extends JFrame implements ActionListener {
 
@@ -150,26 +149,48 @@ public class Login {
             confirmPassword.setBounds(130,250,200,20);
 
             // Creating Buttons
-            JButton Submit = new JButton("SUBMIT");
-            Submit.setBounds(50,325,150,30);
+            JButton back = new JButton("CANCEL");
+            back.setBounds(50,325,150,30);
 
-            JButton back = new JButton("Back");
-            back.setBounds(230,325,160,30);
+            JButton submit = new JButton("SUBMIT");
+            submit.setBounds(230,325,160,30);
 
-//		        if (Submit.getModel().isPressed()) {
-////		        	JOptionPane.showMessageDialog(null, "Your details are now saved!", "Welcome", JOptionPane.INFORMATION_MESSAGE);
-//		        JOptionPane.showInputDialog("Eggs are not supposed to be green.");
-//		        }
-//		        	JOptionPane.showMessageDialog(null,  "Awesome Video Keep it up",
-//		        			"Thank you",JOptionPane.PLAIN_MESSAGE);?
-//		        	back.setEnabled(true);
-//			         newAccount2.setVisible(false);
+            back.addActionListener(new Action1());
+            back.addActionListener(new ActionListener() {
 
+
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+//				JOptionPane.showMessageDialog((Component) e.getSource(), "Your have chosen a non-sustainable energy");
+//		        JOptionPane.setDefaultLocale(null);
+                    String username =enterUsername.getText();
+                    String password = enterPassword.getText();
+                    System.out.println(username);
+                    System.out.println(password);
+
+//					if (back.getModel().isPressed()) {
+//			           newAccount2.setVisible(false);
+//			           newAccount.setVisible(true);
+
+                }});
+
+            back.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    String username =enterUsername.getText();
+                    String password = enterPassword.getText();
+                    System.out.println(username);
+                    System.out.println(password);
+//				        JOptionPane.showMessageDialog((Component) e.getSource(), "Your details are now saved");
+//				        JOptionPane.setDefaultLocale(null);
+
+                }});
 
             JPanel panel2 = new JPanel();
             panel2.add(enterUsername);
             panel2.add(createAccount);
-            panel2.add(Submit);
+            panel2.add(submit);
             panel2.add(back);
             panel2.add(confirmPassword);
             panel2.add(enterPassword);
@@ -189,79 +210,48 @@ public class Login {
             newAccount2.add(enterEmail);
             newAccount2.add(enterPassword);
             newAccount2.add(confirmPassword);
-            newAccount2.add(Submit);
+            newAccount2.add(submit);
             newAccount2.add(back);
             newAccount2.setVisible(true);
             newAccount2.setLocationRelativeTo(null);
             newAccount2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            // back.addActionListener(new Action1());
-            back.addActionListener(new ActionListener() {
-
-
-                @Override
-                public void actionPerformed(ActionEvent ae) {
-                    String username =enterUsername.getText();
-                    String password = enterPassword.getText();
-                    System.out.println(username);
-                    System.out.println(password);
-
-                }});
-
-            Submit.addActionListener(new ActionListener() {
-
-                @Override
-                public void actionPerformed(ActionEvent ae) {
-                    String username = enterUsername.getText();
-                    String password = new String(enterPassword.getPassword());
-                    String confirm = new String(confirmPassword.getPassword());
-                    String email = enterEmail.getText();
-                    String response = Application.createAccount(email, username, password, confirm);
-                    System.out.println(response);
-                }});
-
-
-
             //back.addActionListener(new Action1());
+
             //LoginChoice.addActionListener(new Action1());
             newAccount2.add(panel2); //Add the features in the panel into the frame
 
 
             // Submit.addActionListener(new Action1());
-            Submit.addActionListener(new ActionListener() {
+//				Submit.addActionListener(new ActionListener() {
+//
+////			pressCreateAccount.addActionListener(new Action2());
+////			pressCreateAccount.addActionListener(new ActionListener() {
+//
+//				@Override
+//				public void actionPerformed(ActionEvent ae) {
+//					String username =enterUsername.getText();
+//					String password = enterPassword.getText();
+//					System.out.println(username);
+//					System.out.println(password);
 
-//			pressCreateAccount.addActionListener(new Action2());
-//			pressCreateAccount.addActionListener(new ActionListener() {
+//				}});
 
-                @Override
-                public void actionPerformed(ActionEvent ae) {
-                    String username =enterUsername.getText();
-                    String password = enterPassword.getText();
-                    System.out.println(username);
-                    System.out.println(password);
+//				newAccount2.add(panel2); //Add the features in the panel into the frame
 
-                }});
-
-            newAccount2.add(panel2); //Add the features in the panel into the frame
-
-            //newAccount.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         }
-
 
     }
 }
 
-
-////////////////////////////// FRAME WHEN THE LOGIN BUTTON IS PRESSED //////////////////////////////
-
 //ActionListener once the submit button is pressed
 class Action1 extends JFrame implements ActionListener {
+
 
     public void actionPerformed(ActionEvent e) {
 
         JFrame Frame2 = new JFrame("GO-GREEN");
         Frame2.setSize(600,600);
-        //Frame2.setLayout(new FlowLayout());
         Frame2.setLayout(null);
         Frame2.setLocationRelativeTo(null);
         Frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -274,83 +264,96 @@ class Action1 extends JFrame implements ActionListener {
         JLabel label3 = new JLabel("Please select your food-option:");
         label3.setBounds(200, 50, 250, 25);
 
-        String Food[]={"SELECT:", "Vegetarian-food", "Non-Vegetarian food"};
-        JComboBox SustainableFood = new JComboBox(Food);
-        SustainableFood.setBounds(150,100,300,20);
-        Frame2.add(SustainableFood);
+        JButton Vegetarian = new JButton("Vegetarian meal");
+        Vegetarian.setBounds(100,80,200,30);
+        Vegetarian.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen a Vegetarian meal");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
-        JCheckBox checkbox1 = new JCheckBox();
-        checkbox1.setText("I choose the food-option");
-        checkbox1.setBounds(200,125,170,30);
-        checkbox1.setVisible(true);
-        checkbox1.setSelected(true);
+        JButton NonVegetarian = new JButton("Non-Vegetarian meal");
+        NonVegetarian.setBounds(300,80,200,30);
+        NonVegetarian.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen a non-vegetarian meal");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
         JLabel label4 = new JLabel("Please select your transportation-option:");
         label4.setBounds(200, 150, 250, 25);
 
-        String Transport[]={"SELECT:", "Sustainable Transport", "Non-sustainable transport"};
-        JComboBox SustainableTransport = new JComboBox(Transport);
-        SustainableTransport.setBounds(150,200,300,20);
-        Frame2.add(SustainableTransport);
+        JButton SustainableTransportation = new JButton("Sustainable Transport");
+        SustainableTransportation.setBounds(100,180,210,30);
+        SustainableTransportation.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen Sustainable transport");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
-        JCheckBox checkbox2 = new JCheckBox();
-        checkbox2.setText("I choose the Transport-option");
-        checkbox2.setBounds(200,225,170,30);
-        checkbox2.setVisible(true);
-        checkbox2.setSelected(true);
+        JButton NonSustainableTransportion = new JButton("Non-sustainable transport");
+        NonSustainableTransportion.setBounds(300,180,220,30);
+        NonSustainableTransportion.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen non-sustainable transport");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
         JLabel label5 = new JLabel("Please select your energy-option:");
         label5.setBounds(200, 250, 250, 25);
 
-        String Energy[]={"SELECT:", "Sustainable Energy", "Non-sustainable Energy"};
-        JComboBox SustainableEnergy = new JComboBox(Energy);
-        SustainableEnergy.setBounds(150,300,300,20);
-        Frame2.add(SustainableEnergy);
+        JButton SustainableEnergy = new JButton("Sustainable Energy");
+        SustainableEnergy.setBounds(120,300,200,30);
+        SustainableEnergy.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen sustainable energy");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
-        JCheckBox checkbox3 = new JCheckBox();
-        checkbox3.setText("I choose the energy-option");
-        checkbox3.setBounds(200,350,170,30);
-        checkbox3.setVisible(true);
-        checkbox3.setSelected(true);
+        JButton NonSustainableEnergy = new JButton("Non-Sustainable Energy");
+        NonSustainableEnergy.setBounds(300,300,200,30);
+        NonSustainableEnergy.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog((Component) e.getSource(), "You have chosen a non-sustainable energy");
+                JOptionPane.setDefaultLocale(null);
+            }
+        });
 
-        //Tick Box- Check box
-        JCheckBox checkbox = new JCheckBox();
-        checkbox.setText("Do you wish to proceed?");
-        checkbox.setBounds(200,400,170,30);
-        checkbox.setVisible(true);
-        checkbox.setSelected(true);
-
-        //Button
-        JButton button2 = new JButton("SUBMIT");
-        button2.setSize(100,100);
-        button2.setBounds(210,500,160,50);
-        button2.setEnabled(true);
-        button2.setVisible(true);
+//			        //Button
+//					JButton button2 = new JButton("SUBMIT");
+//					button2.setSize(100,100);
+//					button2.setBounds(210,500,160,50);
+//					button2.setEnabled(true);
+//					button2.setVisible(true);
 
         JPanel panel2 = new JPanel();
         panel2.setLayout(new FlowLayout());
         panel2.add(label3);
         panel2.add(label4);
         panel2.add(label5);
-        panel2.add(checkbox);
-        panel2.add(checkbox1);
-        panel2.add(checkbox2);
-        panel2.add(checkbox3);
-        panel2.add(SustainableFood);
-        panel2.add(SustainableTransport);
+
+//					panel2.add(button2);
+        panel2.add(Vegetarian);
+        panel2.add(SustainableTransportation);
+        panel2.add(NonSustainableTransportion);
         panel2.add(SustainableEnergy);
-        panel2.add(button2);
+        panel2.add(NonSustainableEnergy);
+
         Frame2.add(label3);
         Frame2.add(label4);
         Frame2.add(label5);
-        Frame2.add(SustainableFood);
-        Frame2.add(SustainableTransport);
+        Frame2.add(Vegetarian);
+        Frame2.add(NonVegetarian);
+        Frame2.add(SustainableTransportation);
+        Frame2.add(NonSustainableTransportion);
         Frame2.add(SustainableEnergy);
-        Frame2.add(checkbox);
-        Frame2.add(checkbox1);
-        Frame2.add(checkbox2);
-        Frame2.add(checkbox3);
-        Frame2.add(button2);
+        Frame2.add(NonSustainableEnergy);
+
         Frame2.setVisible(true);
         Frame2.setResizable(true);
         Frame2.setLayout(null);
@@ -358,18 +361,17 @@ class Action1 extends JFrame implements ActionListener {
         Frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Frame2.setBackground(Color.green);
 
-//						if () {
-//
-//						}
+//					button2.addActionListener(new Action2());
+        Vegetarian.addActionListener(new Action2());
+        NonVegetarian.addActionListener(new Action2());
+        SustainableTransportation.addActionListener(new Action2());
+        NonSustainableTransportion.addActionListener(new Action2());
+        SustainableEnergy.addActionListener(new Action2());
+        NonSustainableEnergy.addActionListener(new Action2());
 
-
-        button2.addActionListener(new Action2());
-        // Frame2.add(panel2);
     }
 
-////////////////////////////// FRAME AFTER THE PREVIOUS FRAME///////////////////////////////////////
-
-    static class Action2 extends JFrame implements ActionListener  {
+    class Action2 extends JFrame implements ActionListener  {
 
         public void actionPerformed(ActionEvent e) {
 
@@ -383,12 +385,24 @@ class Action1 extends JFrame implements ActionListener {
             Container c = Frame3.getContentPane();
             c.setBackground(Color.green);
 
+            JLabel label6 = new JLabel("");
+            label6.setBounds(100,30,500,20);
+
+            JPanel panel3 = new JPanel();
+            panel3.setLayout(new FlowLayout());
+            panel3.add(label6);
+            Frame3.add(label6);
+
+            Frame3.setVisible(true);
+            Frame3.setResizable(true);
+            Frame3.setLayout(null);
+            Frame3.setLocationRelativeTo(null);
+            Frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             Frame3.setBackground(Color.green);
-            JPanel panel = new JPanel();
+
             //button2.addActionListener(new Action());
-            Frame3.add(panel); //Add the features in the panel
+            Frame3.add(panel3); //Add the features in the panel
         }
-
-
+//// Gui for demo 2/////////////////////////////////
     }
 }
