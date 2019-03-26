6d35a38c02164ec8b7db620c21d08837da21012c
Himanshu Pathak <h.pathak@student.tudelft.nl>
2019-03-26 10:37:44 +0100

    Added register functionality to new gui

--- src/main/java/client/controllers/RegisterController.java
+++ src/main/java/client/controllers/RegisterController.java
@@ -1,5 +1,6 @@
 package client.controllers;
 
+import client.Application;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
@@ -9,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import objects.RegisterResponse;
 
 import java.io.IOException;
 
@@ -42,7 +44,23 @@ public class RegisterController {
 
     @FXML
     void btnRegister(MouseEvent event) {
+        RegisterResponse response = Application.createAccount(tf_email.getText(),
+            tf_name.getText(), pf_password.getText(), pf_password.getText());
 
+        if (response.getRegisterSuccess() == true) {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Node node = (Node) event.getSource();
+
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+        }
     }
 
 }
