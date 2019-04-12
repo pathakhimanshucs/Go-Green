e0e14d25e34cb00008998317d0f19341df032867
Oliver <oliver.neut@gmail.com>
2019-04-12 13:51:02 +0200

    Fixed activity clarification
    Added a description text to every activity so the user know what in what units he specifies an activity

--- src/main/java/client/controllers/ActivityController.java
+++ src/main/java/client/controllers/ActivityController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -24,39 +25,48 @@ public class ActivityController {
     private MenuButton dropdown;
 
     @FXML
+    private Label description;
+
+    @FXML
     void setBike() {
         activityType = Activity.ActivityObject.BIKE;
-        dropdown.setText("BIKE");
+        dropdown.setText("BIKE (km) ");
+        description.setText("Amount of km travelled by bike (km) : ");
     }
 
     @FXML
     void setVegmeal() {
         activityType = Activity.ActivityObject.VEGMEAL;
-        dropdown.setText("VEGMEAL");
+        dropdown.setText("VEGMEAL (0.2 kg) ");
+        description.setText("Amount of vegetarian meals (portions of 0.2 kg) : " );
     }
 
     @FXML
     void setSolarpanels() {
         activityType = Activity.ActivityObject.SOLARPANELS;
-        dropdown.setText("SOLARPANELS");
+        dropdown.setText("SOLARPANELS (m^2) ");
+        description.setText("Amount of solarpanels installed (1m^2 /panel) : ");
     }
 
     @FXML
     void setLocalfood() {
         activityType = Activity.ActivityObject.LOCALFOOD;
-        dropdown.setText("LOCALFOOD");
+        dropdown.setText("LOCALFOOD (kg) ");
+        description.setText("Mass of local produced food bought (portions of 1 kg) : ");
     }
 
     @FXML
     void setHometemp() {
         activityType = Activity.ActivityObject.HOMETEMP;
-        dropdown.setText("HOMETEMP");
+        dropdown.setText("HOMETEMP (°C) ");
+        description.setText("Amount of degrees the home temperature is lowered in (°C) : ");
     }
 
     @FXML
     void setPubtrans() {
         activityType = Activity.ActivityObject.PUBTRANS;
-        dropdown.setText("PUBTRANS");
+        dropdown.setText("PUBTRANS (km) ");
+        description.setText("Amount of km travelled with public transport (km) : ");
     }
 
 
